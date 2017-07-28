74f1005cc071f10dcf0673402b6233a517b62d6e
RMS <30451119+rms-code@users.noreply.github.com>
2017-07-28 11:01:04 -0400

    updated

--- network_and_storage_core.ps1
+++ network_and_storage_core.ps1
@@ -1,16 +1,7 @@
 ﻿<#
-.SYNOPSIS
-    .
-.DESCRIPTION
-    .
-.PARAMETER Path
-    The path to the .
-.PARAMETER LiteralPath
-    Specifies a path to one or more locations. Unlike Path, the value of 
-    LiteralPath is used exactly as it is typed. No characters are interpreted 
-    as wildcards. If the path includes escape characters, enclose it in single
-    quotation marks. Single quotation marks tell Windows PowerShell not to 
-    interpret any characters as escape sequences.
+This script will ask for a bunch of parameters, look through and make sure you have the information
+
+It will also ask you for node addresses and session identifies for the iscsi part, this output will be ON SCREEN, just copy/paste it in.
 #>
 Param(
 
